01f9289bb50dfa78a061d25f3b39f37e81b9306b
wenzouquan <359945126@qq.com>
2017-08-21 22:09:37 +0800

    12

--- phpDAS_service/Swoole/Server.php
+++ phpDAS_service/Swoole/Server.php
@@ -1,6 +1,6 @@
 <?php
 namespace Swoole\phpkit;
-require 'Swoole/Socket.php';
+require phpkitRoot.'/Swoole/Socket.php';
 class Server {
 	protected $processor = null;
 	protected $ip = "127.0.0.1";
@@ -62,23 +62,53 @@ class Server {
 		return $tMultiplexedProcessor;
 	}
 
+//本项目中的所有服务
+    function getAllServices(){
+        $serviceDir = phpkitRoot."/app/services/";
+        $files=scandir($serviceDir);
+        $serviceNames=[];
+        foreach ($files as $file){
+             if(strpos($file,".php")){
+                  $classname = "\\app\\services\\".str_replace(".php","",$file);
+                  $reflection=new \ReflectionClass($classname);
+                  foreach ( $reflection->getInterfaceNames() as $interface){
+                      if(strpos($interface,"Services")===0){
+                          $reflection=new \ReflectionClass($interface);
+                          $serviceNames[]=$reflection->getNamespaceName();
+                      }
+                  }
+             }
+        }
+        return $serviceNames;
+    }
+
+//生成服务配置
 	function addServiceToConsul() {
-		$serviceName = "Services\Demo\HiService";
-		$this->log("register " . $serviceName);
-		$consul = new \phpkit\consulapi\Consul();
-		$data = array(
-			'ID' => md5($this->ip . ":" . $this->port . "_" . $serviceName),
-			'Name' => $serviceName,
-			'Address' => $this->ip,
-			'Port' => intval($this->config['port']),
-			'Check' => array(
-				'name' => 'status',
-				'tpc' => $this->ip . ":" . $this->port,
-				'Interval' => '5s',
-				'timeout' => '1s',
-			),
-		);
-		$res = $consul->registerServices(json_encode($data));
+        $serviceNames = $this->getAllServices();
+        $config = $this->application->getDi()->getConfig()->get("consul");
+        $data['node_name'] = $config['node_name'];
+        $data['start_join'] = $config['start_join'];
+        $data['data_dir'] = $config['data_dir'];
+        $data['bind_addr'] = $config['bind_addr'];
+		foreach ($serviceNames as $serviceName){
+            $data['services'][] = array(
+                'ID' => md5($this->ip . ":" . $this->config['port']. "_" . $serviceName),
+                'Name' => $serviceName,
+                'Address' => $this->ip,
+                'Port' => intval($this->config['port']),
+                'tags' => [$serviceName,$this->ip,(string)$this->config['port']],
+                'Check' => array(
+                    'name' => 'status',
+                   // 'tpc' => $this->ip . ":" . $this->port,
+                    'tcp' => "$this->ip:$this->port",
+                  //  'http'=>'www.text.je',
+                    'Interval' => '5s',
+                    'timeout' => '1s',
+                ),
+            );
+        }
+        file_put_contents(phpkitRoot . "/config/consul_config.json", json_encode($data));
+        $this->log("生成服务配置文件:consul agent -dev -config-dir=".phpkitRoot . "/config");
 
 	}
 
@@ -88,8 +118,8 @@ class Server {
 		$serv->on('workerStart', [$this, 'onStart']);
 		$serv->on('receive', [$this, 'onReceive']);
 		$serv->set($this->config['set']);
-		// $this->log("注册服务");
-		// $this->addServiceToConsul();
+        $this->log("生成服务配置");
+        $this->addServiceToConsul();
 		$serv->start();
 
 	}
