fab4465ab21f6cfc1ed2d99bf5a5eca34f616fdb
wenzouquan <359945126@qq.com>
2017-08-24 14:45:45 +0800

    12

--- phpDAS_service/dev.php
+++ phpDAS_service/dev.php
@@ -1,15 +1,17 @@
 <?php
+
 $xhprof_enable = 0; //是否性能跟跟踪
 if (function_exists('xhprof_enable') && $xhprof_enable == 1) {
 	xhprof_enable();
 }
+//项目根目录
+define("phpkitRoot", dirname(__FILE__));
 require 'vendor/autoload.php';
-require 'Swoole/Server.php';
+require 'bin/Server.php';
 use phpkit\core\Phpkit as Phpkit;
 use \phpkit\base\BaseController;
 
-//项目根目录
-define("phpkitRoot", dirname(__FILE__));
+
 $phpkit = new Phpkit();
 $setting = require phpkitRoot . '/config/setting.php';
 
@@ -18,6 +20,7 @@ class IndexController extends BaseController {
 		$fire_args = array();
 		$params = $this->dispatcher->getParams();
 		$reflection = new ReflectionMethod($class, $method);
+
 		foreach ($reflection->getParameters() AS $arg) {
 			if ($params[$arg->name]) {
 				$fire_args[$arg->name] = $params[$arg->name];
@@ -26,25 +29,27 @@ class IndexController extends BaseController {
 			}
 
 		}
-		echo '请求参数：';
-		print_r($fire_args);
 		return call_user_func_array(array($class, $method), $fire_args);
 	}
 
-	public function indexAction() {
+    /**
+     * @throws Exception
+     */
+    public function indexAction() {
 		try {
 			$this->getDi()->getRpcClient();
 			$p = explode("/", trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/"));
 			$serverName = "\\app\\services\\" . ucfirst($p[0]);
 			$method = $p[1];
+            //$reflection=new \ReflectionClass($serverName);
+            //var_dump(  $reflection->getInterfaceNames());
 			if (!class_exists($serverName)) {
 				throw new \Exception($serverName . "  class_not_exists", 1);
 			}
 			$service = new $serverName();
 			//$res = $service->$method("wen");
 			$res = $this->fire_theme_method($service, $method);
-			echo '请求返回：';
-			print_r($res);
+            echo json_encode($res);
 		} catch (\Exception $e) {
 			throw new \Exception($e->getMessage());
 		}
--- phpDAS_service/server.php
+++ phpDAS_service/server.php
@@ -1,12 +1,12 @@
 <?php
-require 'vendor/autoload.php';
-require 'Swoole/Server.php';
-use phpkit\core\Phpkit as Phpkit;
 //项目根目录
 define("phpkitRoot", dirname(__FILE__));
+require phpkitRoot.'/vendor/autoload.php';
+require phpkitRoot.'/bin/Server.php';
+use phpkit\core\Phpkit as Phpkit;
 $phpkit = new Phpkit();
 $setting = require phpkitRoot . '/config/setting.php';
 //初始化phpkit
 $app = $phpkit->init($setting);
-$server = new \Swoole\phpkit\Server($app);
+$server = new \bin\phpkit\Server($app);
 $server->serve();
