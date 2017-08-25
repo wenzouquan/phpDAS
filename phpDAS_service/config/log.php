ba7f3cb7a922f116d2fd3ce40fe9f5d11c96ff70
wenzouquan <359945126@qq.com>
2017-08-25 17:13:06 +0800

    12

---
+++ phpDAS_service/config/log.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm. 使用阿里云的日志服务https://www.aliyun.com/product/sls/?spm=5176.8142029.388261.262.ykISLp 服务开通后可以获得下面参数
+ * User: onsakuquan
+ * Date: 17/8/25
+ * Time: 下午5:04
+ */
+
+
+return array(
+    'endpoint' => '',
+    'accessKeyId' => '',
+    'accessKey' => '',
+    'project' => '',
+    'logstore' => '',
+    'token' => "",
+);
